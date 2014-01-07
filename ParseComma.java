810df3ede7b42b523c78098bc5b645050f405b65
Jeremy Schanz <schanz6@yahoo.com>
2014-01-07 02:20:42 -0500

    Update ParseComma.java
    Updates on variable names and added ability to throw NumberFormatException

--- ParseComma.java
+++ ParseComma.java
@@ -1,9 +1,11 @@
 // This program  parse int or double with numbers with commas in it
 
 
-public class ParseComma{
+public class ParseComma
+{
 
- public static double parseDoubleComma(String s){
+ public static double Double(String stringValue)
+ {
   
 	boolean point = false;// used to seperate number after decimal
 	double before =0;// number before decimal 
@@ -12,44 +14,52 @@ public class ParseComma{
 	int i=0;
 	int sign =1;
 	//Check for negative
-	if(s.charAt(0) == '-'){
+	if(stringValue.charAt(0) == '-')
+	{
 	    i++;
 	    sign = -1;
 	}
 	
 	
-	for(;i<s.length();i++){
+	for(;i<stringValue.length();i++)
+	{
 	   //skips comma
-	   if(s.charAt(i)== ','){
+	   if(stringValue.charAt(i) == ',')
+	   {
 		   continue;
-		}
+	   }
            //skips point and switches from before to after		
-	   if(s.charAt(i)=='.'){
-		   if(point){
-		   	return 0;
+	   if(stringValue.charAt(i) == '.')
+	   {
+	   	   // if there is already a point it throws exception 
+		   if(point)
+		   {
+		   	 throw new NumberFormatException();
 		   }
 		   point = true;
 		   
 		   continue;
-         	}
-         	// if not a number returns 0
-         	//help if you know how to return error
-         	if(s.charAt(i)<48||s.charAt(i)>57){
-	   	   return 0;
-	        }
-	      // adds to eithor before or after 
-	   if(point){
-	   	   after = (after*10)+ (s.charAt(i)-48); 
+           }
+         	//throws error if character is not a number
+          if(stringValue.charAt(i)<48||stringValue.charAt(i)>57)
+          {
+	          throw new NumberFormatException();
+	  }
+	      // adds to eithor before or after point total
+	  if(point)
+	  {
+	   	   after = (after*10)+ (stringValue.charAt(i)-48); 
 	   	   aftercount++;
 		
-		
-	            }
-	   else{
-	   	 before = (before*10)+ (s.charAt(i)-48);   
-	   }
+	  }
+	  else
+	  {
+	   	 before = (before*10)+ (stringValue.charAt(i)-48);   
+	  }
 	}
 	// make the after a decimal 
-	for(int j=0;j<aftercount;j++){
+	for(int j=0;j<aftercount;j++)
+	{
 		after = after/10;
 	}
 		
@@ -58,37 +68,42 @@ public class ParseComma{
 	
     }
     // comments are the same as above
-  public static int parseIntComma(String s){
+  public static int Integer(String stringValue)
+  {
   
 	
 	int number =0;
 	int i =0;
 	int sign =1;
 	
-	if(s.charAt(0) == '-'){
+	if(stringValue.charAt(0) == '-')
+	{
 	    i++;
 	    sign = -1;
 	}
 	
 	
 	
-	for(;i<s.length();i++){
+	for(;i<stringValue.length();i++)
+	{
 	
-	   if(s.charAt(i)== ','){
+	   if(stringValue.charAt(i)== ',')
+	   {
 		   continue;
-		}
+	   }
          	
-	   if(s.charAt(i)<48||s.charAt(i)>57){
-			return 0;
-	        }
-	   
-	     number = (number*10)+ (s.charAt(i)-48);   
+	   if(stringValue.charAt(i)<48||stringValue.charAt(i)>57)
+	   {
+	           throw new NumberFormatException();
 	   }
 	   
-	   return sign*number;
-	}
-	
+	     number = (number*10)+ (stringValue.charAt(i)-48);   
+	 }
+	   
+	 return sign*number;
+  }
+}
 		
 	
 
-}
+
